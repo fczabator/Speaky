e84854c7e5873f14c4d0e4f62bb5ca3412b25e0e
Filip Czabator <fczabator@gmail.com>
2019-05-19 21:26:21 +0200

    Added basic apollo server config

---
+++ server/src/main.ts
@@ -0,0 +1,17 @@
+import {ApolloServer, gql} from 'apollo-server';
+import resolvers from './api/resolvers';
+import fs from 'fs';
+import path from 'path';
+
+const typeDefsPath = path.resolve(__dirname, '../api/schema.graphql');
+const typeDefs = gql(fs.readFileSync(typeDefsPath).toString());
+
+const server = new ApolloServer({resolvers, typeDefs});
+
+server.listen().then(({url}) => console.log(`Server ready at ${url}. `));
+
+// Hot Module Replacement
+if (module.hot) {
+  module.hot.accept();
+  module.hot.dispose(() => console.log('Module disposed. '));
+}
