1d5c236508e86ed0c2654f5724c8a52e77a76ef8
Filip Czabator <filip.czabator@vazco.eu>
2019-10-12 21:34:02 +0200

    Added apollo

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,26 @@
 import React, { Component } from 'react';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { AppBar } from './components/AppBar';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Grommet } from 'grommet';
 import { Home } from './screens/Home';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './theme';
-import { AppBar } from './components/AppBar';
-import { Grommet } from 'grommet';
+import { client } from './lib/apollo';
 
-class App extends Component {
-  render() {
-    return (
-      <div style={{ height: '100%' }}>
-        <Grommet>
-          <ThemeProvider theme={theme}>
-            <Router>
-              <AppBar />
-              <Route exact path="/" component={Home} />
-            </Router>
-          </ThemeProvider>
-        </Grommet>
-      </div>
-    );
-  }
-}
+export const App = () => (
+  <ApolloProvider client={client}>
+    <div style={{ height: '100%' }}>
+      <Grommet>
+        <ThemeProvider theme={theme}>
+          <Router>
+            <AppBar />
+            <Route exact path="/" component={Home} />
+          </Router>
+        </ThemeProvider>
+      </Grommet>
+    </div>
+  </ApolloProvider>
+);
 
 export default App;
--- client/src/screens/Home.tsx
+++ client/src/screens/Home.tsx
@@ -1,23 +1,21 @@
 import * as React from 'react';
-import {RoundButton} from '../components/RoundButton';
-import {Screen} from '../components/Screen';
-import {Text} from 'rebass';
-import {RouteComponentProps} from 'react-router';
+import { useQuery } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
+import { RouteComponentProps } from 'react-router';
 
-export const Home: React.FC<RouteComponentProps> = props => (
-  <Screen
-    flex={1}
-    flexDirection="column"
-    justifyContent="center"
-    alignItems="center"
-  >
-    <RoundButton
-      onClick={() => props.history.push('practice')}
-      fontSize={3}
-      variant="primary"
-      width={200}
-    >
-      <Text fontFamily="kalam">Start</Text>
-    </RoundButton>
-  </Screen>
-);
+const FETCH_WORDS = gql`
+  query {
+    words {
+      word
+      _id
+      translate
+    }
+  }
+`;
+
+export const Home: React.FC<RouteComponentProps> = props => {
+  const { data, error, loading } = useQuery(FETCH_WORDS);
+  console.log('data', data);
+
+  return <div>home</div>;
+};
