01461694d0a0562751abdbb9d7fac1e73acf9f4b
Filip Czabator <filip.czabator@vazco.eu>
2019-10-26 12:39:43 +0200

    Added refetch after adding word

---
+++ client/src/api/mutations/createWord.ts
@@ -0,0 +1,11 @@
+import gql from 'graphql-tag';
+
+export default gql`
+  mutation createWord($word: String!, $translate: String!) {
+    createWord(word: $word, translate: $translate) {
+      _id
+      translate
+      word
+    }
+  }
+`;
--- client/src/screens/AddWord.tsx
+++ client/src/screens/AddWord.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
 import { useMutation } from '@apollo/react-hooks';
 import { TextInput, Button } from 'grommet';
-import { gql } from 'apollo-boost';
 import { RouteComponentProps } from 'react-router';
 import { Screen } from '../components/Screen';
+import wordsQuery from '../api/queries/words';
+import createWordMutation from '../api/mutations/createWord';
 
-const ADD_WORD = gql`
-  mutation createWord($word: String!, $translate: String!) {
-    createWord(word: $word, translate: $translate) {
-      _id
-      translate
-      word
-    }
-  }
-`;
-
-export const AddWord: React.FC<RouteComponentProps> = props => {
+export const AddWord: React.FC = () => {
   const [word, setWord] = React.useState('');
   const [translation, setTranslation] = React.useState('');
-  const [createWord] = useMutation(ADD_WORD);
+  const [createWord] = useMutation(createWordMutation, {
+    refetchQueries: [{ query: wordsQuery }]
+  });
 
   const handleAddWord = () => {
     createWord({ variables: { word, translate: translation } });
