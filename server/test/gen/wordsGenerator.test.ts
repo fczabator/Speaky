3389e2f996ae1e3faefccdcc7e8ccab3e3b57da8
Filip Czabator <filip.czabator@vazco.eu>
2019-11-18 21:07:06 +0100

    Changed tests path

--- server/test/gen/wordsGenerator.test.ts
+++ server/test/gen/wordsGenerator.test.ts
@@ -3,6 +3,6 @@ import { generateWords } from '../../src/util/wordsGenerator';
 describe('wordsGenerator', () => {
   it('should draw words for two users', () => {
     const wordIds = ['1', '2', '3', '4'];
-    const drawedWords = generateWords(wordsIds, 2);
+    const drawedWords = generateWords(wordIds, 2);
   });
 });
