91d032b4babb13efc2dc5b301d8354883e83d5cb
Filip Czabator <filip.czabator@vazco.eu>
2019-12-03 09:53:25 +0100

    Added config

--- server/src/config.ts
+++ server/src/config.ts
@@ -3,9 +3,6 @@ export default {
     mocks: 'false',
     playground: process.env.APOLLO_PLAYGROUND !== 'false'
   },
-  jwt: {
-    secret: process.env.JWT_SECRET || 'nope'
-  },
   mongo: {
     retries: +process.env.MONGO_RETRIES || 5,
     url: process.env.MONGO_URL || 'mongodb://localhost:27017/speaky'
@@ -14,7 +11,7 @@ export default {
     port: process.env.SERVER_PORT || 3333
   },
   auth: {
-    domain: 'dev-0xvvzv6k.eu.auth0.com',
-    audience: 'http://localhost:3333'
+    domain: process.env.AUTH_DOMAIN || 'dev-0xvvzv6k.eu.auth0.com',
+    audience: process.env.AUTH_AUDIENCE || 'http://localhost:3333'
   }
 };
