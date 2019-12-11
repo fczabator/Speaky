2a65eca3cc7d2e8051bc6c63f1cbf2dbbc991fdb
Filip Czabator <filip.czabator@vazco.eu>
2019-12-11 18:40:42 +0100

    Removed audience

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -30,7 +30,6 @@ export const App = () => {
           client_id={config.clientId}
           redirect_uri={window.location.origin}
           onRedirectCallback={onRedirectCallback}
-          audience={config.audience}
         >
           <Grommet theme={grommetTheme}>
             <ThemeProvider theme={theme}>
