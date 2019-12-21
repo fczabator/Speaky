1a24e3dd8a3b82251ef3e95c3d7ccbd208a57a83
Filip Czabator <filip.czabator@vazco.eu>
2019-12-21 17:34:21 +0100

    Added not found route

--- client/src/Routes.tsx
+++ client/src/Routes.tsx
@@ -13,27 +13,33 @@ import { Home } from './features/Home';
 import { SelectWords } from './components/SelectWords';
 import { Words } from './features/Words';
 import { AddWordsToChat } from './features/AddWordsToChat';
+import { Route, Switch } from 'react-router-dom';
+
+const NoMatch = () => <div>Not Found :(</div>;
 
 export const Routes = () => {
   return (
     <>
       <AppLayout>
-        <AuthRoute exact path="/" component={Home} />
-        <AuthRoute path="/add-word" component={AddWord} />
-        <AuthRoute path="/words" component={Words} />
-        <AuthRoute
-          exact
-          path="/select-words/:chatId"
-          component={AddWordsToChat}
-        />
-        <AuthRoute path="/select-words" component={SelectWords} />
-        <AuthRoute path="/create-chat" component={CreateChat} />
-        <AuthRoute path="/chat/:_id" component={ChatView} />
-        <AuthRoute path="/chat-invite/:_id" component={ChatInvite} />
-        <AuthRoute path="/chatting/:_id" component={Chatting} />
-        <AuthRoute path="/chats" component={Chats} />
-        <AuthRoute path="/join-chat" component={ChatJoin} />
-        <AuthRoute path="/summary/:_id" component={ChatSummary} />
+        <Switch>
+          <AuthRoute exact path="/" component={Home} />
+          <AuthRoute path="/add-word" component={AddWord} />
+          <AuthRoute path="/words" component={Words} />
+          <AuthRoute
+            exact
+            path="/select-words/:chatId"
+            component={AddWordsToChat}
+          />
+          <AuthRoute path="/select-words" component={SelectWords} />
+          <AuthRoute path="/create-chat" component={CreateChat} />
+          <AuthRoute path="/chat/:_id" component={ChatView} />
+          <AuthRoute path="/chat-invite/:_id" component={ChatInvite} />
+          <AuthRoute path="/chatting/:_id" component={Chatting} />
+          <AuthRoute path="/chats" component={Chats} />
+          <AuthRoute path="/join-chat" component={ChatJoin} />
+          <AuthRoute path="/summary/:_id" component={ChatSummary} />
+          <Route component={NoMatch} />
+        </Switch>
       </AppLayout>
     </>
   );
